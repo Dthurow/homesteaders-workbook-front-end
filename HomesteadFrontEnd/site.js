f4c02cdc2b81f7d9cf118fc8f3d3233fdcbbd86d
Danielle <dan.thurow@gmail.com>
2020-01-31 15:39:45 -0800

    Move site.css, setup express to serve static and transpiled vue code using node site.js command

--- HomesteadFrontEnd/site.js
+++ HomesteadFrontEnd/site.js
@@ -1,89 +1,20 @@
-let uri = '';
+ // set up ========================
+ var express  = require('express');
+ var app      = express();                               // create our app w/ express
+ 
 
-function getContent()
-{
-  console.log('accessing base getContent, not overriden version');
-}
+ // configuration =================
 
-function addItem()
-{
-  const addFormObj = document.getElementById('AddForm');
-  let addedObject = {};
-  for (let i = 0; i < addFormObj.elements.length; i++)
-  {
-    let curElement = addFormObj.elements[i];
-    if (curElement["name"] !== null && curElement["name"] !== "")
-    {
-      addedObject[curElement["name"]] = curElement["value"];
-    }
+ 
+ app.use('/static', express.static('static'));
+ app.use('/dist', express.static('dist'));
 
-  }
+ // application -------------------------------------------------------------
+ app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html'); // load the single view file (vueJS will handle the page changes on the front-end)
+});
 
-  fetch(uri, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(addedObject)
-  })
-    .then(response => response.json())
-    .then(() =>
-    {
-      getContent();
-      for (let i = 0; i < addFormObj.elements.length; i++)
-      {
-        let curElement = addFormObj.elements[i];
-        if (curElement["name"] !== null && curElement["name"] !== "")
-        {
-          curElement["value"] = "";
-        }
-      }
-    })
-    .catch(error => console.error('Unable to add item.', error));
-}
 
-function deleteItem(id)
-{
-  fetch(`${uri}/${id}`, {
-    method: 'DELETE'
-  })
-    .then(() => getContent())
-    .catch(error => console.error('Unable to delete item.', error));
-}
-
-function updateItem()
-{
-
-  const editFormObj = document.getElementById('EditForm');
-  let editedObject = {};
-  for (let i = 0; i < editFormObj.elements.length; i++)
-  {
-    let curElement = editFormObj.elements[i];
-    if (curElement["name"] !== null && curElement["name"] !== "")
-    {
-      editedObject[curElement["name"]] = curElement["value"];
-    }
-
-  }
-
-  fetch(`${uri}/${editedObject.ID}`, {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(editedObject)
-  })
-    .then(() => getContent())
-    .catch(error => console.error('Unable to update item.', error));
-
-  closeInput();
-
-  return false;
-}
-
-function closeInput()
-{
-  document.getElementById('editForm').style.display = 'none';
-}
+ // listen (start app with node server.js) ======================================
+ app.listen(8080);
+ console.log("App listening on port 8080");
