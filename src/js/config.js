2f76fac53e676b22b27c7614013d904ef083a03c
Danielle <dan.thurow@gmail.com>
2020-09-08 11:42:35 -0700

    Update background color

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,6 @@
 export {config};
 
 var config = {
-    "apiURL" : "https://obscure-lowlands-53687.herokuapp.com",
+     "apiURL" : "https://obscure-lowlands-53687.herokuapp.com",
+    //"apiURL" : "http://localhost:5001"
 };
